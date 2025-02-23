6f4cb3494aff899b679519ac39825793de68e886
Daniel Alvarez <daniel.yllanes@hotmail.com>
2025-02-23 13:35:23 -0500

    "/" root route connected

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv')
 dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 6996
+app.get('/',(req,res)=>res.json({"Hello":"World"}))
 app.listen(PORT, ()=>{
     console.log(`Express Server Listening on Port: ${PORT}`)
 })
