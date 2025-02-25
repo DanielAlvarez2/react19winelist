e828d245170e69700c846a43fa31e954424b2d4b
Daniel Alvarez <daniel.yllanes@hotmail.com>
2025-02-25 01:53:45 -0500

    backend api CRUD create functionality working

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@ const express = require('express')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 dotenv.config()
+const Wine = require('./models/Wine.js')
 const app = express()
+app.use(express.json())
 const PORT = process.env.PORT || 6996;
 (async ()=>{
     try{
@@ -12,8 +14,24 @@ const PORT = process.env.PORT || 6996;
         console.log(err)
     }
 })() 
-app.post('/api/wines',(req,res)=>{
-    
+app.post('/api/wines', async (req,res)=>{
+    try{
+        await Wine.create({
+            type:req.body.type,
+            section:req.body.section,
+            category:req.body.category,
+            region:req.body.region,
+            subregion:req.body.subregion,
+            sequence:req.body.sequence,
+            bin:req.body.bin,
+            description:req.body.description,
+            vintageSize:req.body.vintageSize,
+            price:req.body.price            
+        })
+        res.json('Wine Added')
+    }catch(err){
+        console.log(err)
+    }
 })
 app.get('/',(req,res)=>res.json({"Hello":"World"}))
 app.listen(PORT, ()=>{
