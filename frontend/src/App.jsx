cc70a6395b2e6f78dff178aefcb2ce2055d63dbf
Daniel Alvarez <daniel.yllanes@hotmail.com>
2025-02-25 06:52:24 -0500

    set up proxy in /frontend/vite.config.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,14 @@
 import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
 
+  const getWines = async ()=>{
+    await fetch('/api/wines')
+      .then(res=>res.json())
+      .then(json=>console.log(json))
+  }
+  getWines()
   return (
     <>
     test
